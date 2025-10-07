aef66a4352376a37c56227b92bd0ef7aca8f7d25
AlbertC996 <albert996c@gmail.com>
2025-10-07 11:47:12 +0330

    fix all type error currencies service

--- src/currencies/currencies.service.ts
+++ src/currencies/currencies.service.ts
@@ -2,14 +2,25 @@ import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
 
+export interface Currency {
+  ticker: string;
+  name: string;
+  image: string;
+  hasExternalId: boolean;
+  isFiat: boolean;
+  featured: boolean;
+  isStable: boolean;
+  supportsFixedRate: boolean;
+}
+
 @Injectable()
 export class CurrenciesService {
   constructor(private readonly httpService: HttpService) {}
 
-  async getCurrencies() {
+  async getCurrencies(): Promise<Currency[]> {
     try {
       const response = await firstValueFrom(
-        this.httpService.get(
+        this.httpService.get<Currency[]>(
           'https://api.changenow.io/v2/exchange/currencies',
           {
             headers: {
@@ -21,10 +32,32 @@ export class CurrenciesService {
 
       return response.data;
     } catch (error) {
-      console.error(
-        'Error fetching currencies:',
-        error.response?.data || error.message,
-      );
+      let errorMsg = 'Unknown error';
+      if (typeof error === 'object' && error !== null) {
+        // response.data
+        if (
+          'response' in error &&
+          typeof (error as { response?: unknown }).response === 'object' &&
+          (error as { response?: unknown }).response !== null &&
+          'data' in (error as { response: { data?: unknown } }).response
+        ) {
+          const responseObj = (error as { response: { data?: unknown } })
+            .response;
+
+          errorMsg =
+            typeof responseObj.data === 'string'
+              ? responseObj.data
+              : JSON.stringify(responseObj.data);
+        } else if (
+          'message' in error &&
+          typeof (error as { message?: unknown }).message === 'string'
+        ) {
+          errorMsg = (error as { message: string }).message;
+        }
+      } else if (typeof error === 'string') {
+        errorMsg = error;
+      }
+      console.error('Error fetching currencies:', errorMsg);
       throw new Error('Failed to fetch currencies from ChangeNOW');
     }
   }
